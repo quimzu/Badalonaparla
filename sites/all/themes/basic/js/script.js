4d63e320133f39f7c34854cd5fbe1f62c776acc2
Quim Zudaire Prieto <quimzudaire@gmail.com>
2019-01-08 11:29:18 +0100

    HTTPS per github

--- sites/all/themes/basic/js/script.js
+++ sites/all/themes/basic/js/script.js
@@ -1,16 +1,16 @@
 jQuery(document).ready(function($) {
 
 loadImage = new Image();
-loadImage.src = "http://www.badalonaparla.cat/sites/all/themes/basic/images/1.jpg";
+loadImage.src = "https://www.badalonaparla.cat/sites/all/themes/basic/images/1.jpg";
 
 loadImage2 = new Image();
-loadImage2.src = "http://www.badalonaparla.cat/sites/all/themes/basic/images/2.jpg";
+loadImage2.src = "https://www.badalonaparla.cat/sites/all/themes/basic/images/2.jpg";
 
 loadImage3 = new Image();
-loadImage3.src = "http://www.badalonaparla.cat/sites/all/themes/basic/images/3.jpg";
+loadImage3.src = "https://www.badalonaparla.cat/sites/all/themes/basic/images/3.jpg";
 
   //This will be random from bg1.jpg to bg10.jpg
-  /* $("#top").css("background-image", "url(http://www.badalonaparla.cat/sites/all/themes/basic/images/"+GenerateNumber(3)+".jpg)");*/
+  /* $("#top").css("background-image", "url(https://www.badalonaparla.cat/sites/all/themes/basic/images/"+GenerateNumber(3)+".jpg)");*/
 
    //1 to max
    function GenerateNumber(max) {
@@ -21,7 +21,7 @@ $('body,html').scrollTop(505);
 
 $("#navigation li.last a").click(function() {
 		$('body,html').animate({scrollTop : 0},'slow');
- $("#top").css("background-image", "url(http://www.badalonaparla.cat/sites/all/themes/basic/images/"+GenerateNumber(3)+".jpg)");
+ $("#top").css("background-image", "url(https://www.badalonaparla.cat/sites/all/themes/basic/images/"+GenerateNumber(3)+".jpg)");
 	 return false;
 	});
 
